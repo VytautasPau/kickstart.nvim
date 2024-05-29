db0312fcba5d8797bac128d7b56c97a009263841
VytautasPau <pauvytautas@gmail.com>
2024-05-29 08:37:06 +0300

    changes and tabby

--- init.lua
+++ init.lua
@@ -190,6 +190,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Explore shortcut
+vim.keymap.set('n', '<leader>t', ':Explore<CR>', { desc = 'Explore current directory' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -577,6 +580,29 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  maxLineLength = 150,
+                },
+                mccabe = {
+                  enabled = false,
+                },
+                pyflakes = {
+                  enabled = false,
+                },
+                autopep8 = {
+                  enabled = false,
+                },
+                yapf = {
+                  enabled = false,
+                },
+              },
+            },
+          },
+        },
 
         lua_ls = {
           -- cmd = {...},
@@ -885,7 +911,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+vim.g.tabby_keybinding_accept = '<Tab>'
+
+return {
+  { 'TabbyML/vim-tabby' },
+}
