1d669f27ed14d0474511cb7d3ae3d79bb15e9b1d
VytautasPau <pauvytautas@gmail.com>
2024-07-05 10:43:34 +0300

    harpoon added

--- init.lua
+++ init.lua
@@ -291,6 +291,7 @@ require('lazy').setup({
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>a'] = { name = 'Harpoon [A]dd', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
